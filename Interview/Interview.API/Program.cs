093deb63a1f01d8bdd2d3232fa677e22a8c997e6
SwitchAndEko <e880206@gmail.com>
2026-05-14 20:32:17 +0800

    build: 建立 WebAPI 專案

---
+++ Interview/Interview.API/Program.cs
@@ -0,0 +1,51 @@
+namespace Interview.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDatabase(builder.Configuration);
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SchemaFilter<Interview.API.Filters.SwaggerExampleSchemaFilter>();
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BackendExamHub_Context>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("BackendExamHub"))
+            );
+
+            return services;
+        }
+    }
+}
