643926a384fb7b62da6f28bcced5078a4de831ae
SwitchAndEko <e880206@gmail.com>
2026-05-14 20:32:56 +0800

    feat: 建立基底 Controller 以支援 json 格式 request and response

---
+++ Interview/Interview.API/Controllers/GenericController.cs
@@ -0,0 +1,49 @@
+﻿namespace Interview.API.Controllers
+{
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class GenericController : Controller
+    {
+        [NonAction]
+        public IActionResult GenericContent<T>(
+            (BaseFailure? failure, T? data) convertYetResult,
+            Func<T?, IActionResult>? result = null
+        )
+        {
+            if (convertYetResult.failure is not null)
+            {
+                return FailureContent(convertYetResult.failure);
+            }
+            result ??= data => Ok(data);
+            return result(convertYetResult.data);
+        }
+
+        [NonAction]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult FailureContent(BaseFailure failure)
+        {
+            if (string.IsNullOrWhiteSpace(failure.TraceId))
+            {
+                failure.TraceId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            }
+
+            return FailureResultMap(failure);
+        }
+
+        private ObjectResult FailureResultMap(BaseFailure failure)
+        {
+            return failure.Code switch
+            {
+                HttpStatusCode.BadRequest => BadRequest(failure),
+                HttpStatusCode.Unauthorized => Unauthorized(failure),
+                HttpStatusCode.Forbidden => StatusCode((int)HttpStatusCode.Forbidden, failure.Message),
+                HttpStatusCode.NotFound => NotFound(failure),
+                HttpStatusCode.MethodNotAllowed => StatusCode((int)HttpStatusCode.MethodNotAllowed, failure),
+                HttpStatusCode.Conflict => Conflict(failure),
+                HttpStatusCode.Locked => StatusCode((int)HttpStatusCode.Locked, failure),
+                HttpStatusCode.InternalServerError => StatusCode((int)HttpStatusCode.InternalServerError, failure),
+                _ => StatusCode((int)HttpStatusCode.InternalServerError, failure),
+            };
+        }
+    }
+}
